adbb0d5b00ea0d7389251b195aa8329db917217b
jasonjdominguez99 <jasonjdominguez99@users.noreply.github.com>
2023-02-22 00:47:52 +0000

    Create Sphere class method implementations, passing all unit tests

--- src/main/java/Shapes/Sphere.java
+++ src/main/java/Shapes/Sphere.java
@@ -1,10 +1,31 @@
 package Shapes;
 
-public class Sphere implements Shape3D {
-    private double radius;
+import static java.lang.Math.PI;
+import static java.lang.Math.pow;
 
-    public Sphere(double r) {
-        assert false;
+public class Sphere implements Shape3D {
+    private final double radius;
+
+    public Sphere(double r) throws IllegalArgumentException {
+        if (r == 0) {
+            throw new IllegalArgumentException(
+                    "Invalid sphere radius, cannot have a sphere with radius 0"
+            );
+        }
+        if (r < 0) {
+            throw new IllegalArgumentException(
+                    "Invalid sphere radius, cannot have a sphere with a negative radius"
+            );
+        }
+        if (r > 800) {
+            throw new IllegalArgumentException(
+                    String.format(
+                            "Invalid sphere radius, radius %f is too large, much be < 800",
+                            r)
+            );
+        }
+
+        radius = r;
     }
 
     public double getRadius() {
@@ -13,10 +34,10 @@ public class Sphere implements Shape3D {
 
     @Override
     public double surfaceArea() throws IllegalArgumentException {
-        return 0;
+        return 4*PI*pow(radius, 2);
     }
     @Override
     public double volume() throws IllegalArgumentException {
-        return 0;
+        return (4f/3)*PI*pow(radius, 3);
     }
 }
